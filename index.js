1e9450d9af7b04243a809b2de0c9c2a8cab57ea9
Shagun Gupta <shagungupta810@gmail.com>
2025-05-06 06:53:16 +0530

    port changes

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const unzipper = require('unzipper');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
